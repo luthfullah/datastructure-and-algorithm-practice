972f21ffdd74040526b4f94709c75c32be318fc6
luthfullah <luthfullah111@gmail.com>
2024-09-19 23:36:55 +0500

    json stringify practice added

--- jsonPractice.js
+++ jsonPractice.js
@@ -1,10 +1,32 @@
 let userStr = '{"name":"Sammy","email":"sammy@example.com","plan":"Pro"}';
-
+let obj= {
+        "name": "Sammy",
+        "email": "sammy@example.com",
+        "plan": "Pro"
+    }
 let userObj = JSON.parse(userStr);
+let userOb = parseString(obj);
 
 console.log(userObj);
+console.log(userOb);
 // output: {
 //     "name": "Sammy",
 //     "email": "sammy@example.com",
 //     "plan": "Pro"
 // }
+
+
+
+
+let str = '{"name":"Sammy","email":"sammy@example.com","plan":"Pro"}';
+let obje= {
+        "name": "Sammy",
+        "email": "sammy@example.com",
+        "plan": "Pro"
+    }
+let jj=typeof obje==="string" ? obje : JSON.stringify(obje)
+let jso = JSON.parse(str);
+let jsonn = JSON.parse(jj);
+
+console.log(jso);
+console.log(jsonn);
