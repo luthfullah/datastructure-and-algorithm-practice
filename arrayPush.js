62f78bebcdb0d80169548cfb13820811b80d6476
Luthfullah <luthfullah111@gmail.com>
2025-07-23 23:39:37 +0500

    to string added

--- arrayPush.js
+++ arrayPush.js
@@ -1,9 +1,11 @@
-const arr=["x","y"]
-const arr2=["a","b"]
+const arr = ["x", "y"];
+const arr2 = ["a", "b"];
 
-arr2.push(arr)
-console.log(arr2)
-  
+arr2.push(arr);
+const arr3 = arr2.push(arr[0], arr[1]);
+console.log(arr2);
+console.log(arr3);
+
 // output: [
 //     "a",
 //     "b",
